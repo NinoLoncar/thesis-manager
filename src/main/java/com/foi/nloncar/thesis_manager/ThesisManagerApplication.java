93c5ba9d539e4fb25aeb771fd9ab2744a528af76
nloncar <nino.loncar@nth.ch>
2026-08-07 14:09:17 +0200

    project init

---
+++ src/main/java/com/foi/nloncar/thesis_manager/ThesisManagerApplication.java
@@ -0,0 +1,13 @@
+package com.foi.nloncar.thesis_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisManagerApplication.class, args);
+	}
+
+}
